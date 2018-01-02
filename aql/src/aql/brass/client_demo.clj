6469737121cf0ce4580be664bb2b526cf583cd5f
Fredrick Eisele <phreed@gmail.com>
2018-01-02 09:36:03 -0600

    Move client data into data.clj

--- aql/src/aql/brass/client_demo.clj
+++ aql/src/aql/brass/client_demo.clj
@@ -2,37 +2,12 @@
     (:require 
         (org.httpkit [client :as clt])
         (clojure.data [json :as json])
-        (clojure.tools [logging :as log])))
-
-;; https://dsl-external.bbn.com/tracsvr/immortals/browser/trunk/
-;;  docs/CP/Immortals-Phase2-cp1-SchemaMigration.md
-;;  "Sample SubmissionModel value"
-(def schema-mapping
-    { "martiServerModel" 
-        { "requirements"
-            { "postgresqlPerturbation" 
-                { "tables"
-                    [ { "name" "cot_event" 
-                        "columns" 
-                            [   ["cot_event" "source_id"]
-                                ["cot_event" "how"]
-                                ["cot_event" "servertime"]
-                                ["cot_event_position" "point_ce"]
-                                ["cot_event_position" "point_le"]
-                                ["cot_event_position" "tileX"]
-                                ["cot_event_position" "longitude"]
-                                ["cot_event_position" "latitude"]]}
-                    
-                      { "name" "cot_event_detail]"
-                        "columns"
-                              [   ["cot_event_position" "point_hae"]
-                                  ["cot_event" "detail"]
-                                  ["cot_event_position" "tileY"]
-                                  ["cot_event" "cot_type"]]}]}}}})
+        (clojure.tools [logging :as log])
+        (aql.brass [data :as brass])))
 
 
 (def options 
-    {:query-params {:permutation (json/write-str schema-mapping)}})
+    {:query-params {:permutation (json/write-str brass/schema-mapping)}})
         
 (defn -main [& args]
     ;(let [response (clt/post "http://localhost:9090/" "")]
--- aql/src/aql/brass/data.clj
+++ aql/src/aql/brass/data.clj
@@ -7,6 +7,7 @@
         (clojure [pprint :as pp]
                  [string :as st])))
 
+
 ;; https://dsl-external.bbn.com/tracsvr/immortals/browser/trunk/
 ;;   database/server/baseline_schema_ddl.sql
 
@@ -68,7 +69,34 @@
         :entity 
         #{["ce1" "cot_event"]
           ["cot_event_position" "cot_event"]}})
-        
+ 
+
+;; https://dsl-external.bbn.com/tracsvr/immortals/browser/trunk/
+;;  docs/CP/Immortals-Phase2-cp1-SchemaMigration.md
+;;  "Sample SubmissionModel value"
+(def schema-mapping
+    { "martiServerModel" 
+        { "requirements"
+            { "postgresqlPerturbation" 
+                { "tables"
+                    [ { "name" "cot_event" 
+                        "columns" 
+                            [   ["cot_event" "source_id"]
+                                ["cot_event" "how"]
+                                ["cot_event" "servertime"]
+                                ["cot_event_position" "point_ce"]
+                                ["cot_event_position" "point_le"]
+                                ["cot_event_position" "tileX"]
+                                ["cot_event_position" "longitude"]
+                                ["cot_event_position" "latitude"]]}
+                    
+                      { "name" "cot_event_detail]"
+                        "columns"
+                              [   ["cot_event_position" "point_hae"]
+                                  ["cot_event" "detail"]
+                                  ["cot_event_position" "tileY"]
+                                  ["cot_event" "cot_type"]]}]}}}})
+
 ;mapping m3 = literal : s3 -> s3 {
 ;    entity
 ;        Employee -> Employee
