753dbd2cd63f9de7dde8ce8c095842cfdc50bcd1
Fred Eisele <fred.eisele@vanderbilt.edu>
2018-05-01 15:51:40 -0600

    Update to shims #10 They had the wrong types and were missing the opening $$

--- aql_lib/sql/aql_shim.sql
+++ aql_lib/sql/aql_shim.sql
@@ -5,7 +5,7 @@
 
 -- https://www.postgresql.org/docs/10/static/datatype-boolean.html
 
-CREATE FUNCTION OrBool(lhs boolean, rhs boolean) RETURNS bool
+CREATE FUNCTION OrBool(lhs boolean, rhs boolean) RETURNS boolean $$
   BEGIN
      lhs OR rhs
   END;
@@ -14,7 +14,7 @@ $$ LANGUAGE SQL;
 
 -- https://www.postgresql.org/docs/10/static/datatype-character.html
 
-CREATE FUNCTION  eqVc(lhs varchar(16), rhs varchar(16)) RETURNS bool
+CREATE FUNCTION  eqVc(lhs varchar(16), rhs varchar(16)) RETURNS boolean $$
   BEGIN
      lhs = rhs
   END;
